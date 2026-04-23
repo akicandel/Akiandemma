b345cf30faba9af9cc5eb3e5a36e98fd8d43b15f
akicandel <132809356+akicandel@users.noreply.github.com>
2026-04-23 12:40:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const year = new Date().getFullYear();
 document.title = `Emmaki — Creative Studio`;
+<script>
+const thumbs = document.querySelectorAll('.project-thumb');
+
+thumbs.forEach(thumb => {
+  const video = thumb.querySelector('video');
+
+  thumb.addEventListener('mouseenter', () => {
+    video.play();
+  });
+
+  thumb.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0;
+  });
+});
+</script>
