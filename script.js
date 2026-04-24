fb8c00b1c7d0ae524eba3b494ac356bd0c59b855
akicandel <132809356+akicandel@users.noreply.github.com>
2026-04-24 14:30:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.querySelectorAll('.project-thumb').forEach(thumb => {
   const video = thumb.querySelector('video.thumb-gif');
   if (!video) return;
+
   thumb.addEventListener('mouseenter', () => {
     video.currentTime = 0;
     video.play().catch(() => {});
   });
+
   thumb.addEventListener('mouseleave', () => {
     video.pause();
   });
